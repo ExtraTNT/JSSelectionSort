542edda4ac1b2a34c13b1ee18dbacccab1742ba2
ExtraTNT <sven.hugi@swisstxt.ch>
2020-03-04 11:26:50 +0100

    vs code...

--- src/scripts/simple.js
+++ src/scripts/simple.js
@@ -1,4 +1,4 @@
 "use strict";
 const myArr = new Array(9999).fill(0)
-    .map(bla => Math.random() * window.innerWidth);
+    .map(()=> Math.random() * window.innerWidth);
 console.log(myArr.sort((a, b) => a - b));
